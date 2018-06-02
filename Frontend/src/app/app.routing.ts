32de9c5d1ffbfe5f2818824e00558947c8f1120a
Julio Lopez <JulioLopezN>
2018-06-02 15:46:05 -0700

    Archivo de routing

---
+++ Frontend/src/app/app.routing.ts
@@ -0,0 +1,31 @@
+import { Routes } from '@angular/router';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+export const APP_ROUTES: Routes = [
+    {
+        path: 'home',
+        component: HomeComponent,
+        data: { title: 'Home' }
+    },
+    {
+        path: 'login',
+        component: LoginComponent,
+        data: { title: 'Login' }
+    },
+    {
+        path: 'register',
+        component: RegisterComponent,
+        data: { title: 'Register' }
+    },
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/home',
+    },
+    {
+        path: '**',
+        redirectTo: '/home',
+    }
+];
